0a4d5e39ea14ee59d66c281c04a3a2ee23019d40
Kris Wong <krischwong@gmail.com>
2025-05-06 17:52:15 -0400

    Added passport and sessions.

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const express = require("express");
-const memberRouter = require("./routes/memberRouter");
 const path = require("path");
+
+const express = require("express");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple");
+const pgConnect = pgSession(session);
+const pool = require("./db/pool");
+require("dotenv").config();
+
+const memberRouter = require("./routes/memberRouter");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use;
+app.use(
+  session({
+    store: new pgConnect({
+      pool: pool,
+      tableName: "user_sessions",
+      createTableIfMissing: true,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+  })
+);
+
+require("./passport/passport");
 
 app.use("/", memberRouter);
 
---
+++ passport/passport.js
@@ -0,0 +1,46 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+const pool = require("../db/pool");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM member WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM member WHERE user_id = $1",
+      [user_id]
+    );
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
