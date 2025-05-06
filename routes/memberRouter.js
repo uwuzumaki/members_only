eef87bf5442bf56cb088d70013efe98d8cd78e13
Kris Wong <krischwong@gmail.com>
2025-05-06 18:08:21 -0400

    Added all routes for registering and logging in

--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -3,5 +3,9 @@ const memberController = require("../controllers/memberController");
 const memberRouter = Router();
 
 memberRouter.get("/", memberController.homepage);
+memberRouter.get("/register", memberController.registerGet);
+memberRouter.post("/register", memberController.registerPost);
+memberRouter.get("/login", memberController.loginGet);
+memberRouter.post("/login", memberController.loginPost);
 
 module.exports = memberRouter;
