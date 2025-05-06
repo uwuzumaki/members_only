16d0066826f832baa32e6a18e65c078f8867b327
Kris Wong <krischwong@gmail.com>
2025-05-06 18:38:46 -0400

    Can now populate db tables

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,7 +2,25 @@
 require("dotenv").config();
 const { Client } = require("pg");
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE IF NOT EXISTS member (
+  user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR (255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  first_name VARCHAR (255) NOT NULL,
+  last_name VARCHAR (255) not NULL,
+  member_status BOOLEAN NOT NULL DEFAULT FALSE,
+  admin_status BOOLEAN NOT NULL DEFAULT FALSE
+  );
+  
+CREATE TABLE IF NOT EXISTS posts (
+  post_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  poster_id INTEGER REFERENCES member(user_id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  contents TEXT NOT NULL
+  );
+`;
 
 const main = async () => {
   const client = new Client({
