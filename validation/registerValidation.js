e87faee2460d0dd78d2dec7969343c61c70ada55
Kris Wong <krischwong@gmail.com>
2025-05-07 17:58:32 -0400

    Implemented validation for unique username and password validation

---
+++ validation/registerValidation.js
@@ -0,0 +1,30 @@
+const { body, validationResult } = require("express-validator");
+const db = require("../db/queries");
+
+const registrationValidator = [
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 8, max: 64 })
+    .withMessage("Password must be between 8 and 64 characters long"),
+  body("username")
+    .trim()
+    .escape()
+    .custom(async (value) => {
+      const user = await db.getOneMember(value);
+      if (user.length) {
+        throw new Error("Username already in use");
+      }
+      return true;
+    }),
+  body("match_password").custom((value, { req }) => {
+    if (value != req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
+];
+
+module.exports = {
+  registrationValidator,
+};
