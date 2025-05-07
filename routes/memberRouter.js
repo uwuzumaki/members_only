750d82afaa8c8ad303657bdb696078d86dc04b26
Kris Wong <krischwong@gmail.com>
2025-05-07 17:58:01 -0400

    Added validation middleware

--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
 const memberController = require("../controllers/memberController");
+const registerValidation = require("../validation/registerValidation");
 const memberRouter = Router();
 
 memberRouter.get("/", memberController.homepage);
 memberRouter.get("/register", memberController.registerGet);
-memberRouter.post("/register", memberController.registerPost);
+memberRouter.post(
+  "/register",
+  registerValidation.registrationValidator,
+  memberController.registerPost
+);
 memberRouter.get("/login", memberController.loginGet);
 memberRouter.post("/login", memberController.loginPost);
 
