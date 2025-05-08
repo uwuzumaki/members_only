b609ff5aff54b8c1f326cb07a00cb9eae1e4b972
Kris Wong <krischwong@gmail.com>
2025-05-08 15:50:57 -0400

    navbar first draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        screen: "100vw",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "100%",
+        "2xl": "100%",
+      },
+    },
   },
   plugins: ["@tailwindcss/typography"],
 };
